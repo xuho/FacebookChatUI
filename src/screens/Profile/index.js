99a9c9824f1fcd1d501f61c06570fd9199015cdc
Nguyễn Xuân Hoà <hoanx.neonstudio@gmail.com>
2019-11-07 10:11:19 +0700

    Add Settings screen UI

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,26 +1,118 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { profileSection, optionSection, accountAndSupportSection } from './menuData';
 
 // create a component
 class Profile extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Profile</Text>
-            </View>
+            <ScrollView style={styles.container}>
+                <View style={{ justifyContent: 'center', alignItems: 'center', width: '100%', marginBottom: 24 }}>
+                    <View style={styles.avatar}>
+                        <Image source={{
+                            uri: 'http://pngimg.com/uploads/girls/girls_PNG6466.png'
+                        }}
+                            style={{ flex: 1, overflow: 'hidden' }} />
+                    </View>
+                    <Text style={{ fontSize: 24, fontWeight: 'bold', color: '#fff', marginTop: 8 }}>
+                        Nguyễn Xuân Hòa
+                    </Text>
+                </View>
+                <Section
+                    headerTitle='Trang cá nhân'
+                    listItems={profileSection}
+                />
+                <Section
+                    headerTitle='Tùy chọn'
+                    listItems={optionSection}
+                />
+                <Section
+                    headerTitle='Tài khoản và hỗ trợ'
+                    listItems={accountAndSupportSection}
+                />
+                <View style={{ height: 32, backgroundColor: 'transparent' }} />
+            </ScrollView>
         );
     }
 }
 
+const Section = ({ headerTitle, listItems }) => {
+    return (
+        <View style={{ marginBottom: 8 }}>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', color: 'gray', paddingVertical: 16 }}>
+                {headerTitle.toUpperCase()}
+            </Text>
+            {
+                listItems.map((item, index) => {
+                    let { icon, iconBackground, title, rightTitle } = item;
+                    return (
+                        <SectionItem
+                            key={`${headerTitle}-${index}`}
+                            icon={icon}
+                            iconBackground={iconBackground}
+                            title={title}
+                            rightTitle={rightTitle}
+                        />
+                    );
+                })
+            }
+        </View>
+    );
+}
+const SectionItem = ({ icon, iconBackground, title, rightTitle }) => {
+    return (
+        <TouchableOpacity
+            activeOpacity={0, 7}
+            style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', paddingVertical: 8 }}>
+            <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+                <View style={{
+                    width: 36, height: 36, borderRadius: 18, backgroundColor: iconBackground,
+                    flexDirection: 'row', alignItems: 'center', marginRight: 16
+                }}>
+                    <Icon
+                        style={{ flex: 1, textAlign: 'center' }}
+                        size={18}
+                        color='#fff'
+                        name={icon}
+                    />
+                </View>
+                <Text style={{ fontSize: 16, color: '#fff' }}>
+                    {title}
+                </Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                {
+                    rightTitle
+                        ? <Text style={{ fontSize: 16, color: 'gray', marginRight: 8 }}>
+                            {rightTitle}
+                        </Text>
+                        : null
+                }
+                <Icon
+                    name='chevron-right'
+                    size={24}
+                    color='gray'
+                />
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const AVATAR_SIZE = 80;
 // define your styles
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        backgroundColor: '#000',
+        paddingTop: 32,
+        paddingHorizontal: 16
     },
+    avatar: {
+        width: AVATAR_SIZE, height: AVATAR_SIZE, borderRadius: AVATAR_SIZE / 2,
+        overflow: 'hidden', borderWidth: 3, borderColor: '#fff'
+    }
 });
 
 //make this component available to the app
---
+++ src/screens/Profile/menuData.js
@@ -0,0 +1,92 @@
+export const profileSection = [
+    {
+        icon: 'nintendo-switch',
+        iconBackground: '#6cd64f',
+        title: 'Trạng thái hoạt động',
+        rightTitle: 'Bật'
+    },
+    {
+        icon: 'at',
+        iconBackground: '#f94262',
+        title: 'Tên người dùng',
+        rightTitle: 'm.me/xuho'
+    },
+    {
+        icon: 'cellphone-iphone',
+        iconBackground: '#28a3ff',
+        title: 'Điện thoại',
+        rightTitle: 'Thêm số điện thoại'
+    },
+];
+
+export const optionSection = [
+    {
+        icon: 'bell',
+        iconBackground: '#773aff',
+        title: 'Thông báo và âm thanh',
+        rightTitle: 'Bật'
+    },
+    {
+        icon: 'account-multiple',
+        iconBackground: '#30bfff',
+        title: 'Danh bạ'
+    },
+    {
+        icon: 'file',
+        iconBackground: '#28a3ff',
+        title: 'Tin'
+    },
+    {
+        icon: 'lock',
+        iconBackground: '#333333',
+        title: 'Cuộc trò chuyện bí mật'
+    },
+    {
+        icon: 'sd',
+        iconBackground: '#6cd64f',
+        title: 'Dữ liệu và bộ nhớ'
+    },
+    {
+        icon: 'visual-studio',
+        iconBackground: '#131ca8',
+        title: 'Trợ lý ảo'
+    },
+    {
+        icon: 'emoticon-happy',
+        iconBackground: '#f94186',
+        title: 'Biểu tượng cảm xúc'
+    },
+];
+
+export const accountAndSupportSection = [
+    {
+        icon: 'account-switch',
+        iconBackground: '#773aff',
+        title: 'Chuyển tài khoản'
+    },
+    {
+        icon: 'account-plus',
+        iconBackground: '#868e99',
+        title: 'Tạo tài khoản mới'
+    },
+    {
+        icon: 'settings',
+        iconBackground: '#28a3ff',
+        title: 'Cài đặt tài khoản'
+    },
+    {
+        icon: 'alert',
+        iconBackground: '#f96c56',
+        title: 'Báo cáo sự cố'
+    },
+    {
+        icon: 'lifebuoy',
+        iconBackground: '#30bfff',
+        title: 'Trợ giúp'
+    },
+    {
+        icon: 'file-document',
+        iconBackground: '#868e99',
+        title: 'Pháp lý và chính sách'
+    },
+];
